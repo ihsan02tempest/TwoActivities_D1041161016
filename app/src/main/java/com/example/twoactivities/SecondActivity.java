69a7f4a77302e82076d53d3536b752612bafd70c
ihsan <ihsan.maulana90@gmail.com>
2019-09-26 10:30:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+    }
+}
